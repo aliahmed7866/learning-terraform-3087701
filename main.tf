49fba1710346dcb967e11f261039888e6bc3262f
Ali Ahmed <sibkhan131@gmail.com>
2023-06-11 23:21:32 +0100

    used miduke for security group

--- main.tf
+++ main.tf
@@ -35,7 +35,18 @@ resource "aws_security_group_rule" "blog_http_in"{
 
   security_group_id = aws_security_group.blog.id
 }
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name = "blog_new"
+  vpc_id= data.aws_vpc.default.id
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks= ["0.0.0.0/0"]
 
+  egress_rules = ["all-all"]
+  egress_cidr_blocks= ["0.0.0.0/0"]
+
+}
 resource "aws_security_group_rule" "blog_https_in"{
   type = "ingress"
   from_port = 443
@@ -59,7 +70,7 @@ resource "aws_security_group_rule" "blog_everything_out"{
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
 
   tags = {
