758364edf84018f61610238bebfc654d95b8a3f0
Ali Ahmed <sibkhan131@gmail.com>
2023-06-12 23:06:03 +0100

    syntax correction

--- variables.tf
+++ variables.tf
@@ -1,15 +1,17 @@
 variable "ami-filter" {
-  description =  "Name filter aand owner of AMI"
+  description =  "Name filter and owner of AMI"
   
   type =  object({
     name = string 
-    owner =string
+    owner = string
   })
   
   default = {
     name = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
     owner = "979382823631" # Bitnami
   }
+}
+
 variable "instance-type" {
   description = "Type of EC2 instance to provision"
   default = "t3.nano"
@@ -18,24 +20,23 @@ variable "instance-type" {
 variable "environment" {
   description = "development environment"
   
-  type = "object ({
-  name = string
-  network_prefix =string
+  type = object({
+    name = string
+    network_prefix = string
   })
+
   default = {
     name = "dev"
-    network-prefix = "10.0"
+    network_prefix = "10.0"
   }
 }
-
 
 variable "asg_min_size" {
   description = "Minimum number of instances in ASG"
   default = 1
-  }
+}
+
 variable "asg_max_size" {
   description = "Maximum number of instances in ASG"
   default = 2
-  }
- 
-
+}
